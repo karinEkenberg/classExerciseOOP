9be485471769662bac64704d36afb84363415cb6
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-01-12 19:52:11 +0100

    Add files via upload

---
+++ classExerciseOOP/Car.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classExerciseOOP
+{
+    internal class Car
+    {
+        //public string regNr;
+        //public string make;
+        //public string model;
+        //public int year;
+        //public bool forSale;
+
+        private string regNr;
+        private string make;
+        private string model;
+        private int year;
+        private bool forSale;
+
+        public string RegNr
+        {
+            get { return regNr; }
+            set { regNr = value; }
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set 
+            {
+                if ((value < 1990))
+                {
+                    year = -1;
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+        }
+
+        public bool ForSale
+        {
+            get { return forSale; }
+            set { forSale = value; }
+        }
+
+        public String YearToString()
+        {
+            if(this.year == -1)
+            {
+                return "Incorrect year";
+            }
+            else
+            {
+                return Convert.ToString(year);
+            }
+        }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string regNr, string make, string model, int year, bool forSale)
+        {
+            this.regNr = regNr;
+            this.make = make;   
+            this.model = model;
+            this.year = year;
+            this.forSale = forSale;
+        }
+
+        public override String ToString()
+        {
+            return String.Format("\nCar Info\nReg: {0}, {1} {2} [{3}]\n{4}",
+                this.regNr, this.make, this.model, this.YearToString(), this.ForSaleToString());
+        }
+
+        public String ForSaleToString()
+        {
+            if (this.ForSale)
+            {
+                return "Car is for sale.";
+            }
+            else
+            {
+                return "Car is not for sale.";
+            }
+        }
+    }
+}
---
+++ classExerciseOOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using classExerciseOOP;
+
+Car c1 = new Car();
+Car c2 = new Car("DEF345", "Saab", "88", 2010, false);
+//c1.regNr = "ABC123";
+//c1.make = "Volvo";
+//c1.model = "V40";
+//c1.year = 1990;
+//c1.forSale = true;
+c1.RegNr = "ABC123";
+c1.Make = "Volvo";
+c1.Model = "V40";
+c1.Year = 19;
+c1.ForSale = true;
+Console.WriteLine("Enter cars registration number:");
+string regNr = Console.ReadLine();
+Console.WriteLine("Enter cars make:");
+string make = Console.ReadLine();
+Console.WriteLine("Enter cars model:");
+string model = Console.ReadLine();
+Console.WriteLine("Enter cars production year:");
+int year = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Is it for sale Y/N");
+char ch = Convert.ToChar(Console.ReadLine());
+bool forsale = false;
+if(Char.ToUpper(ch) == 'J')
+{
+    forsale = true;
+}
+Car c = new Car(make, model, regNr, year, forsale);
+Console.WriteLine("-------------------------");
+Console.WriteLine("[Regnr] [Make & modell]");
+Console.WriteLine("-------------------------");
+Console.WriteLine($"{c1.RegNr} : {c1.Make} {c1.Model}");
+Console.WriteLine(c1.ToString());
+Console.WriteLine(c2.ToString());
+Console.WriteLine(c.ToString());
+
